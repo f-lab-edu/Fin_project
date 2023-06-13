de0c1d0212cca7c76ef9921486c824fa97dca1de
HeesuYeo <gmltnwkd123@naver.com>
2023-06-13 21:39:00 +0900

    fix: git.ignore 수정
    git.ignore 수정

---
+++ login/jspLogin/src/main/java/com/example/jsplogin/models/User.java
@@ -0,0 +1,17 @@
+package com.example.jsplogin.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String id;
+    private String password;
+
+    public User(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+}
